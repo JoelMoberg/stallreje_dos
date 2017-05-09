e40807a4069599442163905fe6afc97b74e70c3e
Cazei <Carl@carica.com>
2017-05-09 13:31:26 +0200

    Responsive menu fix

--- meny.php
+++ meny.php
@@ -22,5 +22,31 @@
         ?>
       </ul>
       <a href="javascript:void(0);" class="hamburger" id="toggleMenu"><i class="fa fa-bars" aria-hidden="true" ></i></a>
-
 </div>
+
+<div class="responsiveMenu is-center">
+  <div class="vertical-middle">
+    <ul class="pure-menu-list navigation" >
+      <?php
+        $args = array(
+          'sort_order' => 'ASC',
+          'sort_column' => 'menu_order',
+          'hierarchical' => 1,
+          'exclude' => '79 109 120 118 119',
+          'child_of' => 0,
+          'parent' => -1,
+          'exclude_tree' => '',
+          'number' => '',
+          'offset' => 0,
+          'post_type' => 'page',
+          'post_status' => 'publish'
+          );
+          $pages = get_pages($args);
+          foreach($pages as $page){
+            echo '<li class="pure-menu-item"><a href="'.get_page_link( $page->ID).'">'.$page->post_title.'</a></li>';
+          }
+        ?>
+        <a href="javascript:void(0);" class="close" id="close"><i class="fa fa-times"  aria-hidden="true"></i></a>
+        </ul>
+      </div>
+    </div>
